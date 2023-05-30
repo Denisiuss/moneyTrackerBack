604d22b57223c770fe19cc9fe28e0a3aa2551393
John Doe <johndoe@email.com>
2023-05-30 14:37:38 +0300

    dock

--- src/main/java/sideProject/moneyTracker/Repositories/PaymentsRepo.java
+++ src/main/java/sideProject/moneyTracker/Repositories/PaymentsRepo.java
@@ -20,40 +20,40 @@ public interface PaymentsRepo extends JpaRepository <Payments, Long> {
 
     @Transactional
     @Modifying (clearAutomatically = true)
-    @Query(value = "SELECT * FROM moneytrackerapp.payments WHERE CATEGORIES = :categories AND customer_id = :customerId order by purchase_date desc Limit 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM payments WHERE CATEGORIES = :categories AND customer_id = :customerId order by purchase_date desc Limit 1", nativeQuery = true)
     ArrayList<Payments> findLastPayment (@Param("customerId") long customerId, @Param("categories") String categories);
 
     @Transactional
     @Modifying (clearAutomatically = true)
-    @Query(value = "SELECT * FROM moneytrackerapp.payments WHERE CATEGORIES = :categories AND customer_id = :customerId order by purchase_date desc Limit 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM payments WHERE CATEGORIES = :categories AND customer_id = :customerId order by purchase_date desc Limit 1", nativeQuery = true)
     ArrayList<Payments> findLastIncome (@Param("customerId") long customerId, @Param("categories") String categories);
 
     @Transactional
     @Modifying (clearAutomatically = true)
-    @Query(value =  "select sum(amount) as amount, id, categories, purchase_date, customer_id, receipt, title\n" +
-                    "from payments where customer_id = :customerId AND `purchase_date` >= DATE_SUB(CURDATE(), INTERVAL 6 MONTH) AND  categories= :categories  \n" +
-                    "group  by DATE_FORMAT(purchase_date, '%Y %M')\n" +
-                    "order by DATE(purchase_date);", nativeQuery = true)
+    @Query(value =  "SELECT date_trunc('month', purchase_date) as purchase_date, sum(amount) as amount, max(id) as id, categories, sum(customer_id) as customer_id, max(receipt) as receipt, max(title) as title\n\n" +
+                    "from payments where customer_id = :customerId AND (purchase_date >= CURRENT_DATE - INTERVAL '6 months') AND  categories= :categories  \n" +
+                    "group by 1, 4\n" +
+                    "order by 1;", nativeQuery = true)
     ArrayList<Payments> findLastSixMonthsIncomes (@Param("customerId") long customerId, @Param("categories") String categories);
 
     @Transactional
     @Modifying (clearAutomatically = true)
-    @Query(value =  "select sum(amount) as amount, id, categories, purchase_date, customer_id, receipt, title\n" +
-            "from payments where customer_id = :customerId AND `purchase_date` >= DATE_SUB(CURDATE(), INTERVAL 1 day) AND  categories= :categories  \n" +
-            "group  by DATE_FORMAT(purchase_date, '%Y %M');", nativeQuery = true)
+    @Query(value =  "SELECT date_trunc('month', purchase_date) as purchase_date, sum(amount) as amount, max(id) as id, categories, sum(customer_id) as customer_id, max(receipt) as receipt, max(title) as title\n" +
+            "from payments where customer_id = :customerId AND (purchase_date >= CURRENT_DATE - INTERVAL '1 day') AND  categories= :categories  \n" +
+            "group by 1, 4;", nativeQuery = true)
     ArrayList<Payments> findTodayIncomes (@Param("customerId") long customerId, @Param("categories") String categories);
 
     @Transactional
     @Modifying (clearAutomatically = true)
-    @Query(value =  "select sum(amount) as amount, id, categories, purchase_date, customer_id, receipt, title\n" +
-            "from payments where customer_id = :customerId AND `purchase_date` >= DATE_SUB(CURDATE(), INTERVAL 1 week) AND  categories= :categories  \n" +
-            "group  by DATE_FORMAT(purchase_date, '%Y %M');", nativeQuery = true)
+    @Query(value =  "SELECT date_trunc('month', purchase_date) as purchase_date, sum(amount) as amount, max(id) as id, categories, sum(customer_id) as customer_id, max(receipt) as receipt, max(title) as title\n" +
+            "from payments where customer_id = :customerId AND (purchase_date >= CURRENT_DATE - INTERVAL '1 week') AND  categories= :categories  \n" +
+            "group by 1, 4;", nativeQuery = true)
     ArrayList<Payments> findWeekIncomes (@Param("customerId") long customerId, @Param("categories") String categories);
 
     @Transactional
     @Modifying (clearAutomatically = true)
-    @Query(value =  "select sum(amount) as amount, id, categories, purchase_date, customer_id, receipt, title\n" +
-            "from payments where customer_id = :customerId AND `purchase_date` >= DATE_SUB(CURDATE(), INTERVAL 1 Month) AND  categories= :categories  \n" +
-            "group  by DATE_FORMAT(purchase_date, '%Y %M');", nativeQuery = true)
+    @Query(value =  "SELECT date_trunc('month', purchase_date) as purchase_date, sum(amount) as amount, max(id) as id, categories, sum(customer_id) as customer_id, max(receipt) as receipt, max(title) as title\n" +
+            "from payments where customer_id = :customerId AND (purchase_date >= CURRENT_DATE - INTERVAL '1 Month') AND  categories= :categories  \n" +
+            "group by 1, 4;", nativeQuery = true)
     ArrayList<Payments> findMonthIncomes (@Param("customerId") long customerId, @Param("categories") String categories);
 }
